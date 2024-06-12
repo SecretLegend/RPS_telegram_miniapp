ddad83a231ac92beea4ff9a2bfc29a0086a08a4e
Secretlegend <97430835+SecretLegend@users.noreply.github.com>
2024-06-12 21:58:44 +0900

    fix: minor change on bot

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@
 import { Telegraf, Markup, Context } from "telegraf";
 
 console.log("Token:", process.env.TELEGRAM_BOT_TOKEN)
-const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN || "6018859569:AAG17oJTs-GeYr2t8diNiv5VyqOHKT2ELHs");
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN || "");
 
 bot.command('start', ctx => {
   ctx.reply("Welcome to Rock-Paper-Scissors Game!\nHere you can play rock-paper-scissors game.\nClick the button below or go to the chat which you want to send the invitation to, type in @RockPaperScissorsGame_Minibot, and add a space.",
@@ -12,7 +12,7 @@ bot.command('start', ctx => {
         inline_keyboard: [
             [
                 {
-                    text: msg.t("button.play"),
+                    text: "Play",
                     switch_inline_query_chosen_chat: {
                         query: "",
                         allow_bot_chats: false,
